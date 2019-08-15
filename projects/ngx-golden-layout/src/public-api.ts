7d8835d656edbf0ba3740e5df9ff9e0fc3ca2bc7
Anthony Mandra <anthony.mandra@keysight.com>
2019-08-15 09:42:32 -0400

    missing exports

--- projects/ngx-golden-layout/src/public-api.ts
+++ projects/ngx-golden-layout/src/public-api.ts
@@ -1,6 +1,7 @@
 export * from './lib/config';
+export * from './lib/component-registry.service';
 export * from './lib/golden-layout.component';
-export { PluginRegistryService } from './lib/plugin-registry.service';
+export * from './lib/plugin-registry.service';
 export * from './lib/root-window.service';
 export * from './lib/hooks';
 export * from './lib/module';
