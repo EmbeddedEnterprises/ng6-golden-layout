51afff86bfe2eb6ac7adbda124046996c5aa3f80
Martin Koppehel <martin.koppehel@st.ovgu.de>
2019-09-25 18:20:13 +0200

    Disable close for the created dummy stack, prevent adding content items to the dummy stack

--- projects/ngx-golden-layout/src/lib/wrapper.component.ts
+++ projects/ngx-golden-layout/src/lib/wrapper.component.ts
@@ -1,12 +1,32 @@
-import { Component, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation, Inject, Injectable } from '@angular/core';
+import { GlHeaderItem } from './hooks';
+import { GoldenLayoutComponentHost, GoldenLayoutComponentState } from './tokens';
 
 @Component({
   selector: 'gl-wrapper',
   encapsulation: ViewEncapsulation.None,
   template: `<div class="wrapper"></div>`
 })
-export class WrapperComponent {
-  constructor() {
+export class WrapperComponent implements GlHeaderItem {
+  get headerComponent() {
+    if (!this.originalComponent || !this.originalComponent.instance) {
+      return undefined;
+    }
+    return this.originalComponent.instance.then(x => x.instance.headerComponent);
+  }
+  get additionalTokens() {
+    if (!this.originalComponent || !this.originalComponent.instance) {
+      return undefined;
+    }
+    return this.originalComponent.instance.then(x => x.instance.additionalTokens);
+  }
 
+  private originalComponent: any;
+
+  constructor(
+    @Inject(GoldenLayoutComponentHost) private host: any,
+    @Inject(GoldenLayoutComponentState) private state: any,
+  ) {
+    this.originalComponent = (this.host.getGoldenLayoutInstance() as any)._getAllComponents()[this.state.originalId];
   }
 }
